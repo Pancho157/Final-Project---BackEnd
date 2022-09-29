ce1a444b4574c5dd67f598ead148875c4223330f
Pancho157 <hoxocraft65@gmail.com>
2022-09-29 18:54:34 -0300

    Modificada la estructura del proyecto y agregada la conexión a firebase

---
+++ Entrega 10 (2da Proyecto Final)/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { Server: HttpServer } = require("http");
+
+var app = express();
+const httpServer = new HttpServer(app);
+// const io = new IOServer(httpServer);
+
+const PORT = process.env.PORT || 8080;
+const server = httpServer.listen(PORT, () => {
+  console.log(`Http - Socket Server On - Port: ${PORT}`);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ----------------------- Router -----------------------
+app.use("/api/productos", require("./routes/apiProductos"));
+app.use("/api/carrito", require("./routes/apiCarrito"));
+
+// ----------------------- Error 404 -----------------------
+app.use((req, res) => {
+  res.status(404).send({
+    error: -2,
+    descripción: `ruta ${req.url} método ${req.method} no implementada`,
+  });
+});
