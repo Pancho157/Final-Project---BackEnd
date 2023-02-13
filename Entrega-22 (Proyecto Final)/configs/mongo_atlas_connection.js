8500d89de9bf1dfed15cb1eb6a566f9e771be9d6
Pancho157 <hoxocraft65@gmail.com>
2023-02-12 23:14:12 -0300

    Final project - Separated production and development BBDDs

--- Entrega-22 (Proyecto Final)/configs/mongo_atlas_connection.js
+++ Entrega-22 (Proyecto Final)/configs/mongo_atlas_connection.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const { logger } = require("./logger");
 
 function connectToMongo() {
-  mongoose.connect(process.env.MONGO_URL, {
+  let connectionURL =
+    process.env.ENV == "prod"
+      ? process.env.MONGO_ATLAS_URL
+      : process.env.MONGO_LOCAL_URL;
+
+  mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
--- Entrega-22 (Proyecto Final)/configs/sessions_connection.js
+++ Entrega-22 (Proyecto Final)/configs/sessions_connection.js
@@ -1,9 +1,14 @@
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
+const connectionURL =
+  process.env.ENV == "prod"
+    ? process.env.MONGO_ATLAS_URL
+    : process.env.MONGO_LOCAL_URL;
+
 const Session = session({
   store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URL,
+    mongoUrl: connectionURL,
   }),
   secret: "top_secret_secret_key",
   resave: true,
